77872dc6d64f2050d1e242c2882b8d3967b5a7f8
dv-jamie <sjm2506@gmail.com>
2022-07-10 21:44:42 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import Pusher from 'pusher-js';
+import ChatList from './components/ChatList';
+import ChatBox from './components/ChatBox';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      username: '',
+      chats: []
+    };
+  }
+
+  componentDidMount() {
+    const pusher = new Pusher('APP_KEY', {
+      cluster: 'APP_CLUSTER',
+      encrypted: true
+    });
+    const channel = pusher.subscribe('chat');
+    channel.bind('message', data => {
+      this.setState({ chats: [...this.state.chats, data], test: '' });
+    });
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(e) {
+    if (e.keyCode === 13) {
+      const payload = {
+        username: this.state.username,
+        message: this.state.text
+      };
+      axios.post('http://localhost:3000/messages', payload);
+    } else {
+      this.setState({ text: e.target.value });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React-Pusher Chat</h1>
+        </header>
+        <section>
+          <ChatList chats={this.state.chats} />
+          <ChatBox
+            text={this.state.text}
+            handleTextChange={this.handleTextChange}
+          />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
