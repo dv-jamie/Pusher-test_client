4076372d276573f0bcf345cbeb0b4341d135f440
dv-jamie <sjm2506@gmail.com>
2022-07-11 18:41:17 +0900

    send message 업데이트

--- src/App.js
+++ src/App.js
@@ -1,59 +1,72 @@
-import React, { Component } from 'react';
 import axios from 'axios';
 import Pusher from 'pusher-js';
-import ChatList from './components/ChatList';
-import ChatBox from './components/ChatBox';
+import { useEffect, useState } from 'react';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: '',
-      username: '',
-      chats: []
+function App() {
+  const pusher = new Pusher("8c8c1f6d8d719dd3d233", {
+    cluster: "ap3"
+  })
+
+  const [chats, setChats] = useState([])
+  const [onlineUserCount, setOnlineUserCount] = useState()
+  const [text, setText] = useState("")
+
+  // useEffect 안에 적을 것 = 클래스형 컴포넌트의 componentDidMount()의 역할
+  useEffect(() => {
+    // subscribe(연결)
+    const channel = pusher.subscribe('chat')
+
+    // channel.bind("pusher:subscription_succeeded", (members) => {
+    //   console.log("count", members.count)
+    // })
+
+    // channel.bind("pusher:member_added", (member) => {
+    //   console.log("add", channel.members.count)
+    // })
+
+    // 서버에서 메시지 생성 후 response => 받은 데이터를 chats에 추가
+    channel.bind("create-message", data => {
+      const { username, message } = data
+      setChats((prev) => [
+        ...prev, { username, message }
+      ])
+    })
+  }, [])
+
+  // 메시지 전송 버튼 클릭 => 서버로 데이터 전송
+  const clickSendButton = async () => {
+    const payload = {
+        username: "정민",
+        message: text
     };
+  
+    await axios.post('http://localhost:3000/pusher/messages', payload);
   }
 
-  componentDidMount() {
-    const pusher = new Pusher('APP_KEY', {
-      cluster: 'APP_CLUSTER',
-      encrypted: true
-    });
-    const channel = pusher.subscribe('chat');
-    channel.bind('message', data => {
-      this.setState({ chats: [...this.state.chats, data], test: '' });
-    });
-    this.handleTextChange = this.handleTextChange.bind(this);
-  }
-
-  handleTextChange(e) {
-    if (e.keyCode === 13) {
-      const payload = {
-        username: this.state.username,
-        message: this.state.text
-      };
-      axios.post('http://localhost:3000/messages', payload);
-    } else {
-      this.setState({ text: e.target.value });
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React-Pusher Chat</h1>
-        </header>
-        <section>
-          <ChatList chats={this.state.chats} />
-          <ChatBox
-            text={this.state.text}
-            handleTextChange={this.handleTextChange}
+  return (
+    <div>
+      <section>
+        <div>
+          <input
+            placeholder="chat here..."
+            onChange={ e => { setText(e.target.value) } }
           />
-        </section>
-      </div>
-    );
-  }
+          <button onClick={ clickSendButton }>전송</button>
+        </div>
+
+        <ul>
+          {chats.map((chat, index) => {
+              return (
+                  <div key={index}>
+                      <strong>{chat.username}</strong>
+                      <p>{chat.message}</p>
+                  </div>
+              );
+          })}
+        </ul>
+      </section>
+    </div>
+  )
 }
 
 export default App;
